a5a027950a45bfe335a505b1894c456dc8df2550
Andrew Klimek <andrew.klimek@gmail.com>
2017-06-03 12:28:22 -0400

    add taxonomy processing to description

--- minimalist-seo.php
+++ minimalist-seo.php
@@ -45,8 +45,9 @@ function mnmlseo_schema() {
 		$post_author = get_userdata( $data->post_author );
 		$description = get_post_meta( $data->ID, 'mnmlseo_description', true );
 		if ( ! $description ) {
-			$description = get_the_excerpt( $data->ID );
-			poo($data->post_excerpt,'$description');
+			// the codex doesn't say, but this won't auto-generate excerpts outside the loop
+			// Good in this case, the first 55 chars would be a crappy meta description
+			$description = $data->post_excerpt;
 		}
 	
 		$metadata = array(
@@ -83,10 +84,55 @@ function mnmlseo_schema() {
 		echo '<script type="application/ld+json">'. json_encode( $metadata ) .'</script>';
 	}
 	if ( $description ) {
+		
+		$description = mnmlseo_process_meta( $description, $data->ID );
+		
 		echo '<meta name="description" content="'. esc_attr( $description ) .'">';
 	}
 }
 add_action( 'wp_head', 'mnmlseo_schema' );
+
+
+function mnmlseo_custom_title() {
+	$id = get_the_ID();
+	$title = get_post_meta( $id, 'mnmlseo_title', true );
+	
+	if ( ! $title ) {
+		return '';
+	}
+
+	$title = str_ireplace( '[title]', single_post_title( '', false ), $title );// why not get_the_title, or global post?
+	
+	$title = mnmlseo_process_meta( $title, $id );
+	
+	return $title;
+}
+add_filter( 'pre_get_document_title', 'mnmlseo_custom_title' );
+
+function mnmlseo_process_meta( $content, $id ) {
+	
+	if ( false !== strpos( $content, '[' ) ) {
+		
+		$content = str_ireplace(
+						array( '[tag]', '[tags]', '[category]', '[categories]' ),
+						array( '[tax:tag]', '[tax:tag]', '[tax:category]', '[tax:category]' ),
+						$content
+		);
+
+		if ( false !== strpos( $content, '[tax:' ) ) {
+		
+			$content = preg_replace_callback(
+							'/\[tax:(\w+?)\]/',
+							function ( $match ) {
+								return implode( ', ', array_map( function($v){ return $v->name; }, get_the_terms( $id, $match[1] ) ) );
+							},
+							$content
+			);
+		}
+	}
+	return $content;
+}
+
 
 /**
  * Adds a box to the main column on the Post and Page edit screens.
@@ -118,7 +164,7 @@ function mnmlseo_seo_meta_box_callback( $post, $metabox ) {
 	 * Use get_post_meta() to retrieve an existing value
 	 * from the database and use the value for the form.
 	 */
-	$title = get_post_meta( $post->ID, 'mnmlseo_custom_title', true );
+	$title = get_post_meta( $post->ID, 'mnmlseo_title', true );
 	if ( ! $title ) $title = '';
 
 	echo '';
@@ -128,12 +174,21 @@ function mnmlseo_seo_meta_box_callback( $post, $metabox ) {
 
 	echo '
 		<p><span class="description">The size of these fields shows where Google would cut off the text.  You can type beyond but it probably won’t display in search results.</span></p>
-		<p><label for="mnmlseo_custom_title">Custom Title: </label></p>
-		<textarea id="mnmlseo_custom_title" name="mnmlseo_custom_title" style="width:35.5em; max-width:100%; height: 1.9em; overflow-y: scroll;" placeholder="default is “Post Title - Site Name”">' . esc_attr( $title ) . '</textarea>
-		<span class="description">You can use [title] to automatically insert the post title and [tax:your_taxonomy] for a string of terms, e.g., “[title], a post about [tax:tags]”</span>
+		<p><label for="mnmlseo_title">Custom Title: </label></p>
+		<textarea id="mnmlseo_title" name="mnmlseo_title" style="width:35.5em; max-width:100%; height: 1.9em; overflow-y: scroll;" placeholder="default is “Post Title - Site Name”">' . esc_attr( $title ) . '</textarea>
+		<div class="description">You can use [title] to automatically insert the post title and [tax:your_taxonomy] for a string of terms, e.g., “[title], a post about [tax:tags]”</div>
 		<p><label for="mnmlseo_description">Meta Description: </label></p>
 		<textarea id="mnmlseo_description" name="mnmlseo_description" placeholder="Uses post excerpt by default. Type here to customize." style="word-break:break-all; width:40.5em; max-width:100%; height:3.3em; overflow-y: scroll;">' . esc_attr( $description ) . '</textarea>
-		<span class="description">Google will sometimes use the meta description of a page in search results snippets, if they think it gives users a more accurate description than would be possible purely from the on-page content. <a href="https://support.google.com/webmasters/answer/35624#1" rel="nofollow">[read more]</a></span>';
+		<div class="description">Google will sometimes use the meta description of a page in search results snippets, if they think it gives users a more accurate description than would be possible purely from the on-page content. <a href="https://support.google.com/webmasters/answer/35624#1" rel="nofollow">[read more]</a></div>
+		<div id="mnmlseo-preview-title"></div><div id="mnmlseo-preview-desc"></div>';
+		// <script>
+		// 	var t = document.getElementById("mnmlseo-preview-title"), d = document.getElementById("mnmlseo-preview-desc"), pt = document.getElementById("title"),
+		// 	mt = document.getElementById("mnmlseo_title");
+		// 	t.innerHTML = pt.value;
+		// 	pt.addEventListener("input", function(){ if(!mt.value) t.innerHTML=this.value});
+		// 	mt.addEventListener("input", function(){t.innerHTML=this.value});
+		// 	document.getElementById("mnmlseo_description").addEventListener("input", function(){d.innerHTML=this.value});
+		// </script>
 }
 
 /**
@@ -180,10 +235,10 @@ function mnmlseo_save_seo_meta_box_data( $post_id ) {
 	/* OK, it's safe for us to save the data now. */
 
 	// Make sure that it is set.
-	if ( ! empty( $_POST['mnmlseo_custom_title'] ) ) {
+	if ( ! empty( $_POST['mnmlseo_title'] ) ) {
 		// Sanitize user input... is it needed?
 		// Update the meta field in the database.
-		update_post_meta( $post_id, 'mnmlseo_custom_title', sanitize_text_field( $_POST['mnmlseo_custom_title'] ) );
+		update_post_meta( $post_id, 'mnmlseo_title', sanitize_text_field( $_POST['mnmlseo_title'] ) );
 	}
 	if ( ! empty( $_POST['mnmlseo_description'] ) ) {
 		
@@ -193,37 +248,3 @@ function mnmlseo_save_seo_meta_box_data( $post_id ) {
 	
 }
 add_action( 'save_post', 'mnmlseo_save_seo_meta_box_data' );
-
-function mnmlseo_custom_title() {
-	$id = get_the_ID();
-	$title = get_post_meta( $id, 'mnmlseo_custom_title', true );
-	
-	if ( ! $title ) {
-		return '';
-	}
-
-	$title = str_ireplace( '[title]', single_post_title( '', false ), $title );// why not get_the_title, or global post?
-	
-	if ( false !== strpos( $title, '[' ) ) {
-		
-		$title = str_ireplace(
-						array( '[tag]', '[tags]', '[category]', '[categories]' ),
-						array( '[tax:tag]', '[tax:tag]', '[tax:category]', '[tax:category]' ),
-						$title
-		);
-
-		if ( false !== strpos( $title, '[tax:' ) ) {
-		
-			$title = preg_replace_callback(
-							'/\[tax:(\w+?)\]/',
-							function ( $match ) {
-								return implode( ', ', array_map( function($v){ return $v->name; }, get_the_terms( $id, $match[1] ) ) );
-							},
-							$title
-			);
-		}
-	}
-	
-	return $title;
-}
-add_filter( 'pre_get_document_title', 'mnmlseo_custom_title' );
